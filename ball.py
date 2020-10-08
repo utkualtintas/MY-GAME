79a262ac7cf4a7ea9027e598d9219183ce498c73
utku <utku@Utku-MacBook-Pro.local>
2020-10-09 00:00:17 +0300

    I am trying to learn  git workflow

--- ball.py
+++ ball.py
@@ -19,7 +19,7 @@ class TheBall:
         self.set_player_interventions()
         self.deal_with_scoring()
         self.put_the_goalpost_physics()
-        self.update_player_positions()
+        self.update_player_posiitions()
 
     def put_me_on_the_screen(self, screen):
         screen.blit(self.surface, (self.rectangle.left, self.rectangle.top))
